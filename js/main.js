5972d7a580a1f2e4341837c4140ae977048052bd
pierfrancescoscar <scarinci401@gmail.com>
2021-11-17 11:14:45 +0100

    ADD: activeIndex in data setted on 0

--- js/main.js
+++ js/main.js
@@ -9,9 +9,13 @@
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
+
 const app = new Vue ({
     el: '#app',
     data: {
+
+        activeIndex: 0,
+        
         contacts: [
             {
                 name: 'Michele',
