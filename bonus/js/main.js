98423bcef75f24d42db37ce699b5aa8245e7150c
pierfrancescoscar <scarinci401@gmail.com>
2021-11-18 14:34:44 +0100

    ADD: static info/delete msgs div with his styling

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -12,6 +12,12 @@
 // Milestone 4
 // Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 
+// Milestone 5 - opzionale
+// - Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+
+// - Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti 
+
+
 
 
 
--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@
 // Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 
 
+
+
 const app = new Vue ({
     el: '#app',
     data: {
